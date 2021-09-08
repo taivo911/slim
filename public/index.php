28609f3e0fea74d49ab82b82ee2dd75143b78455
user <taivo.nurk@khk.ee>
2021-09-08 21:56:07 +0300

    Installed and configured dependency injection

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
